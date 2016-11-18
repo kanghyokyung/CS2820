1d0513fc0c7b9bfa9e30825faaca183dcc300932
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-17 22:50:02 -0500

    Create Orders.java
    this is  the orders/

---
+++ Orders.java
@@ -0,0 +1,52 @@
+package warehouse;
+
+
+
+import java.util.HashMap;
+
+/**
+*@author HyoKyung Kang
+*
+/
+
+
+public class Orders {
+	
+	String progress;
+	String address;
+	String dateReceived;
+	int orderNumber;
+	HashMap<String, Integer> Item;
+	public Orders(String progress,String address, String dateReceived,int orderNumber){
+		progress=progress;
+		address=address;
+		dateReceived=dateReceived;
+		this.orderNumber=orderNumber;
+		this.Item= new HashMap<String,Integer>();
+		
+	}
+	
+	
+	
+	public void ship(String dateReceived){
+	
+	}
+	public void cancel(){
+		
+	}
+	
+	public void changeProgress(){
+		
+	}//
+	public void returningprocess(){
+		
+	}
+	public void addItems(){
+		
+	}
+	public void getProgress(){
+		
+	}//
+	
+	
+}
