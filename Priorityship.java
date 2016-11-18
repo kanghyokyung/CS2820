48dd5b062e831713c5ab297eaf8663ec669749c7
hyo kyung kang <hyokyung-kang@uiowa.edu>
2016-11-17 22:59:08 -0500

    Create Priorityship.java

---
+++ Priorityship.java
@@ -0,0 +1,12 @@
+
+
+public class Priorityship extends Orders{
+
+	public Priorityship(String progress, String address, String dateReceived, int orderNumber) {
+		super(progress, address, dateReceived, orderNumber);
+		
+		
+		//there is overwriting the things you can rewrite the method again!@_@ 
+	}
+
+}
